5b2925af43e378a255dafa5965749edbf121fd35
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-11-18 11:45:09 -0600

    Initial Commit

---
+++ TestingInteroperability/TestingInteroperability.h
@@ -0,0 +1,18 @@
+//
+//  TestingInteroperability.h
+//  TestingInteroperability
+//
+//  Created by Jax DesMarais-Leder on 11/18/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestingInteroperability.
+FOUNDATION_EXPORT double TestingInteroperabilityVersionNumber;
+
+//! Project version string for TestingInteroperability.
+FOUNDATION_EXPORT const unsigned char TestingInteroperabilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingInteroperability/PublicHeader.h>
+
+
